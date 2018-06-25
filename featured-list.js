9ca46e32990296fab869ffae7d06a9363fc8290d
Samuel Rapana Betio <samuelbetio@users.noreply.github.com>
2018-06-25 17:25:56 +0900

    Add files via upload

---
+++ featured-list.js
@@ -0,0 +1,21 @@
+var featuredList = [{
+	name: 'srcset',
+	description: 'Parse and stringify the HTML <img> srcset attribute',
+	website: 'https://github.com/sindresorhus/srcset'
+}, {
+	name: 'detect-indent',
+	description: 'Detect the indentation of code',
+	website: 'https://github.com/sindresorhus/detect-indent'
+}, {
+	name: 'stringify-object',
+	description: 'Stringify an object using a custom indentation',
+	website: 'https://github.com/yeoman/stringify-object'
+}, {
+	name: 'devtools-detect',
+	description: 'Detect if DevTools is open',
+	website: 'https://github.com/sindresorhus/devtools-detect'
+}, {
+	name: 'URI.js',
+	description: 'URL mutation library',
+	website: 'http://medialize.github.io/URI.js/'
+}];
