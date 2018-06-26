04338251e5e3c1d9c4c418b94ce5d89c0a51938c
Samuel Rapana Betio <samuelbetio@users.noreply.github.com>
2018-06-26 16:40:04 +0800

    Update featured-list.js

--- featured-list.js
+++ featured-list.js
@@ -1,11 +1,11 @@
 var featuredList = [{
 	name: 'srcset',
 	description: 'Parse and stringify the HTML <img> srcset attribute',
-	website: 'https://github.com/sindresorhus/srcset'
+	website: 'https://github.com/samuelbetio/GreenFairTemplate/tree/srcset'
 }, {
 	name: 'detect-indent',
 	description: 'Detect the indentation of code',
-	website: 'https://github.com/sindresorhus/detect-indent'
+	website: 'https://github.com/samuelbetio/GreenFairTemplate/tree/detect-indent'
 }, {
 	name: 'stringify-object',
 	description: 'Stringify an object using a custom indentation',
@@ -13,9 +13,9 @@ var featuredList = [{
 }, {
 	name: 'devtools-detect',
 	description: 'Detect if DevTools is open',
-	website: 'https://github.com/sindresorhus/devtools-detect'
+	website: 'https://github.com/samuelbetio/GreenFairTemplate/tree/stringify-object'
 }, {
 	name: 'URI.js',
 	description: 'URL mutation library',
-	website: 'http://medialize.github.io/URI.js/'
+	website: 'https://samuelbetio.github.io/URI.js/'
 }];
